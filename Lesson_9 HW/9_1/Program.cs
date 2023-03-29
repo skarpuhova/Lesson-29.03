cd7a70d2161b5d6361d6796f8233c8e27807f50c
Светлана <skarpuhova@mail.ru>
2023-03-29 17:18:11 +0300

    Task 9_1HW

---
+++ Lesson_9 HW/9_1/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Write("Введите n: ");
+int n = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введите m: ");
+int m = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine($"n={n}, m={m}");
+if (m > n)
+    for (int i = n; i <= m; i++)
+        Console.Write($" {i}");
+else
+    for (int i = m; i <= n; i++)
+        Console.Write($" {i}");
